3d29770df49818cd2a80b0d76fced8ce5c4650ea
tom <tom@codecreator.net>
2018-08-30 00:34:04 +0200

    Source of first version

---
+++ B8Stack/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace A7Stack
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CGoButton = new System.Windows.Forms.Button();
+            this.CStepSize = new System.Windows.Forms.ComboBox();
+            this.CStepDirection = new System.Windows.Forms.ComboBox();
+            this.LStepSize = new System.Windows.Forms.Label();
+            this.LStepDirection = new System.Windows.Forms.Label();
+            this.CFrames = new System.Windows.Forms.NumericUpDown();
+            this.LFrames = new System.Windows.Forms.Label();
+            this.CCycleTime = new System.Windows.Forms.NumericUpDown();
+            this.LCycleTime = new System.Windows.Forms.Label();
+            this.CProgress = new System.Windows.Forms.ProgressBar();
+            this.CAboutButton = new System.Windows.Forms.Button();
+            this.CRunTimer = new System.Windows.Forms.Timer(this.components);
+            this.CDryRun = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CFrames)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CCycleTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CGoButton
+            // 
+            this.CGoButton.Location = new System.Drawing.Point(15, 251);
+            this.CGoButton.Name = "CGoButton";
+            this.CGoButton.Size = new System.Drawing.Size(185, 37);
+            this.CGoButton.TabIndex = 0;
+            this.CGoButton.Text = "Go";
+            this.CGoButton.UseVisualStyleBackColor = true;
+            this.CGoButton.Click += new System.EventHandler(this.CGoButton_Click);
+            // 
+            // CStepSize
+            // 
+            this.CStepSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CStepSize.Items.AddRange(new object[] {
+            "small",
+            "normal",
+            "big"});
+            this.CStepSize.Location = new System.Drawing.Point(128, 16);
+            this.CStepSize.Name = "CStepSize";
+            this.CStepSize.Size = new System.Drawing.Size(121, 21);
+            this.CStepSize.TabIndex = 0;
+            // 
+            // CStepDirection
+            // 
+            this.CStepDirection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CStepDirection.FormattingEnabled = true;
+            this.CStepDirection.Items.AddRange(new object[] {
+            "far to near",
+            "near to far"});
+            this.CStepDirection.Location = new System.Drawing.Point(128, 54);
+            this.CStepDirection.Name = "CStepDirection";
+            this.CStepDirection.Size = new System.Drawing.Size(121, 21);
+            this.CStepDirection.TabIndex = 1;
+            // 
+            // LStepSize
+            // 
+            this.LStepSize.AutoSize = true;
+            this.LStepSize.Location = new System.Drawing.Point(12, 19);
+            this.LStepSize.Name = "LStepSize";
+            this.LStepSize.Size = new System.Drawing.Size(50, 13);
+            this.LStepSize.TabIndex = 3;
+            this.LStepSize.Text = "Step size";
+            this.LStepSize.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LStepDirection
+            // 
+            this.LStepDirection.AutoSize = true;
+            this.LStepDirection.Location = new System.Drawing.Point(12, 57);
+            this.LStepDirection.Name = "LStepDirection";
+            this.LStepDirection.Size = new System.Drawing.Size(72, 13);
+            this.LStepDirection.TabIndex = 4;
+            this.LStepDirection.Text = "Step direction";
+            this.LStepDirection.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // CFrames
+            // 
+            this.CFrames.Location = new System.Drawing.Point(129, 94);
+            this.CFrames.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.CFrames.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.CFrames.Name = "CFrames";
+            this.CFrames.Size = new System.Drawing.Size(120, 20);
+            this.CFrames.TabIndex = 5;
+            this.CFrames.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // LFrames
+            // 
+            this.LFrames.AutoSize = true;
+            this.LFrames.Location = new System.Drawing.Point(12, 96);
+            this.LFrames.Name = "LFrames";
+            this.LFrames.Size = new System.Drawing.Size(41, 13);
+            this.LFrames.TabIndex = 6;
+            this.LFrames.Text = "Frames";
+            this.LFrames.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // CCycleTime
+            // 
+            this.CCycleTime.Location = new System.Drawing.Point(129, 144);
+            this.CCycleTime.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.CCycleTime.Minimum = new decimal(new int[] {
+            750,
+            0,
+            0,
+            0});
+            this.CCycleTime.Name = "CCycleTime";
+            this.CCycleTime.Size = new System.Drawing.Size(120, 20);
+            this.CCycleTime.TabIndex = 7;
+            this.CCycleTime.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // LCycleTime
+            // 
+            this.LCycleTime.AutoSize = true;
+            this.LCycleTime.Location = new System.Drawing.Point(12, 146);
+            this.LCycleTime.Name = "LCycleTime";
+            this.LCycleTime.Size = new System.Drawing.Size(77, 13);
+            this.LCycleTime.TabIndex = 8;
+            this.LCycleTime.Text = "Cycle time (ms)";
+            this.LCycleTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // CProgress
+            // 
+            this.CProgress.Location = new System.Drawing.Point(15, 222);
+            this.CProgress.Name = "CProgress";
+            this.CProgress.Size = new System.Drawing.Size(234, 23);
+            this.CProgress.TabIndex = 9;
+            // 
+            // CAboutButton
+            // 
+            this.CAboutButton.Location = new System.Drawing.Point(207, 251);
+            this.CAboutButton.Name = "CAboutButton";
+            this.CAboutButton.Size = new System.Drawing.Size(42, 37);
+            this.CAboutButton.TabIndex = 10;
+            this.CAboutButton.Text = "?";
+            this.CAboutButton.UseVisualStyleBackColor = true;
+            this.CAboutButton.Click += new System.EventHandler(this.CAboutButton_Click);
+            // 
+            // CRunTimer
+            // 
+            this.CRunTimer.Tick += new System.EventHandler(this.CRunTimer_Tick);
+            // 
+            // CDryRun
+            // 
+            this.CDryRun.AutoSize = true;
+            this.CDryRun.Checked = true;
+            this.CDryRun.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CDryRun.Location = new System.Drawing.Point(15, 177);
+            this.CDryRun.Name = "CDryRun";
+            this.CDryRun.Size = new System.Drawing.Size(156, 17);
+            this.CDryRun.TabIndex = 11;
+            this.CDryRun.Text = "Only focus step, no capture";
+            this.CDryRun.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 309);
+            this.Controls.Add(this.CDryRun);
+            this.Controls.Add(this.CAboutButton);
+            this.Controls.Add(this.CProgress);
+            this.Controls.Add(this.LCycleTime);
+            this.Controls.Add(this.CCycleTime);
+            this.Controls.Add(this.LFrames);
+            this.Controls.Add(this.CFrames);
+            this.Controls.Add(this.LStepDirection);
+            this.Controls.Add(this.LStepSize);
+            this.Controls.Add(this.CStepDirection);
+            this.Controls.Add(this.CStepSize);
+            this.Controls.Add(this.CGoButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "B8 Stack 0.01";
+            ((System.ComponentModel.ISupportInitialize)(this.CFrames)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CCycleTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CGoButton;
+        private System.Windows.Forms.ComboBox CStepSize;
+        private System.Windows.Forms.ComboBox CStepDirection;
+        private System.Windows.Forms.Label LStepSize;
+        private System.Windows.Forms.Label LStepDirection;
+        private System.Windows.Forms.NumericUpDown CFrames;
+        private System.Windows.Forms.Label LFrames;
+        private System.Windows.Forms.NumericUpDown CCycleTime;
+        private System.Windows.Forms.Label LCycleTime;
+        private System.Windows.Forms.ProgressBar CProgress;
+        private System.Windows.Forms.Button CAboutButton;
+        private System.Windows.Forms.Timer CRunTimer;
+        private System.Windows.Forms.CheckBox CDryRun;
+    }
+}
+
---
+++ B8Stack/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace A7Stack
+{
+    enum ERunState
+    {
+        Idle,
+        Running
+    }
+
+    public partial class MainForm : Form
+    {
+        // ancient win api functions that we need
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, IntPtr className, string windowTitle);
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, uint lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        // some constants from win api
+        static uint wm_keydown = 0x0100;
+        static uint wm_keyup = 0x0101;
+        static uint wm_char = 0x0102;
+
+        // state
+        private ERunState RState = ERunState.Idle;
+        private int CaptureCount = 0;
+        private int CaptureCountTarget = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            CStepSize.SelectedIndex = 1;
+            CStepDirection.SelectedIndex = 1;
+            CProgress.Value = 0;
+        }
+
+        private void CGoButton_Click(object sender, EventArgs e)
+        {
+            switch(RState)
+            {
+                case ERunState.Idle:
+                    ToRunning();
+                    break;
+
+                case ERunState.Running:
+                    ToIdle();
+                    break;
+            }
+        }
+
+        private void CRunTimer_Tick(object sender, EventArgs e)
+        {
+            switch(RState)
+            {
+                case ERunState.Idle:
+                    CRunTimer.Stop();
+                    break;
+
+                case ERunState.Running:
+                    FrameTick();
+                    break;
+            }
+        }
+
+        private void ToIdle()
+        {
+            RState = ERunState.Idle;
+            CRunTimer.Stop();
+
+            CGoButton.Text = "Go";
+            CProgress.Value = 0;
+            CCycleTime.Enabled = true;
+            CStepSize.Enabled = true;
+            CStepDirection.Enabled = true;
+            CDryRun.Enabled = true;
+            CFrames.Enabled = true;
+
+            CaptureCount = 0;
+            CaptureCountTarget = 0;
+        }
+
+        private void ToRunning()
+        {
+            CGoButton.Text = "Stop";
+            CProgress.Value = 0;
+            CProgress.Maximum = (int)CFrames.Value;
+            CRunTimer.Interval = (int)CCycleTime.Value;
+            CCycleTime.Enabled = false;
+            CStepSize.Enabled = false;
+            CStepDirection.Enabled = false;
+            CDryRun.Enabled = false;
+            CFrames.Enabled = false;
+
+            CaptureCount = 0;
+            CaptureCountTarget = (int)CFrames.Value;
+
+            RState = ERunState.Running;
+            CRunTimer.Start();
+        }
+
+        private void FrameTick()
+        {
+            // get remote imagine edge window and find the focus button inside it
+            IntPtr dialogClass = IntPtr.Zero;
+            dialogClass += 0x8002;
+            IntPtr hwnd = FindWindowEx(IntPtr.Zero, IntPtr.Zero, dialogClass, "Remote");
+            if(hwnd == IntPtr.Zero)
+            {
+                ToIdle();
+                MessageBox.Show(this,  "Unable to find main window, please start image edge remote application.", "B8 Stack", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            hwnd = FindWindowEx(hwnd, IntPtr.Zero, "Button", string.Empty);
+            if (hwnd == IntPtr.Zero)
+            {
+                ToIdle();
+                MessageBox.Show(this, "Unable to find control window, this version of imagine edge is not supported.", "B8 Stack", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            // send key sequence
+            SimulateUserInput(hwnd, CStepSize.SelectedIndex, CStepDirection.SelectedIndex, (int)CCycleTime.Value, CDryRun.Checked);
+
+            ++CProgress.Value;
+            ++CaptureCount;
+            if (CaptureCount == CaptureCountTarget)
+                ToIdle();
+        }
+
+        private void SimulateUserInput(IntPtr hwnd, int stepSize, int stepDir, int cycleTime, bool dryRun)
+        {
+            int keyCode = 0x00;
+            if(stepDir == 0)
+            {
+                switch(stepSize)
+                {
+                    case 0: keyCode = 0x45; break;
+                    case 1: keyCode = 0x57; break;
+                    case 2: keyCode = 0x51; break;
+                }
+            }
+            else if(stepDir == 1)
+            {
+                switch (stepSize)
+                {
+                    case 0: keyCode = 0x52; break;
+                    case 1: keyCode = 0x54; break;
+                    case 2: keyCode = 0x59; break;
+                }
+            }
+
+            if(keyCode != 0)
+            {
+                //SetForegroundWindow(hwnd);
+                SendPressSequence(hwnd, keyCode);
+                Thread.Sleep(500); // give the sony some time to do the adjustment
+
+                if (!dryRun)
+                {
+                    //SetForegroundWindow(hwnd);
+                    SendPressSequence(hwnd, 0x31);
+                }
+            }
+        }
+
+        private void SendPressSequence(IntPtr hwnd, int keyCode)
+        {
+            // simulate up, char and down window messages
+            uint lParamDown = (0x00000001 | (MapVirtualKey((uint)keyCode, (uint)0) << 16));
+            uint lParamUp = (0xC0000001 | (MapVirtualKey((uint)keyCode, (uint)0) << 16));
+
+            PostMessage(hwnd, wm_keydown, keyCode, lParamDown);
+            PostMessage(hwnd, wm_char, keyCode, lParamDown);
+            PostMessage(hwnd, wm_keyup, keyCode, lParamUp);
+        }
+
+        private void CAboutButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("B8 Stack by binarycamera\nbinarycamera@codecreator.net");
+        }
+    }
+}
